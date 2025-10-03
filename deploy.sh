dd4932690bfad8321957a91adeda7b01199bd536
Mihai Bucse <mihaibucse@gmail.com>
2025-10-03 12:52:29 +0100

    use pnpm in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 set -e
 
-# Paths
-CMS_SRC="apps/cms"
-WEB_SRC="apps/web"
+# Define paths
+ROOT_DIR=$(pwd)
+CMS_SRC="$ROOT_DIR/apps/cms"
+WEB_SRC="$ROOT_DIR/apps/web"
 CMS_TARGET="/home/totk/admin.totunik.ro"
 WEB_TARGET="/home/totk/new.totunik.ro"
 
 echo "=== Building Strapi (CMS) ==="
 cd $CMS_SRC
-npm install --production=false
-npm run build
+pnpm install --prod=false
+pnpm build
 
 echo "=== Deploying Strapi to $CMS_TARGET ==="
 rm -rf $CMS_TARGET/*
 cp -r . $CMS_TARGET/
-cd -
+cd $ROOT_DIR
 
 echo "=== Building Next.js (Web) ==="
 cd $WEB_SRC
-npm install --production=false
-npm run build
-npm run export   # ensures static export into "out/"
+pnpm install --prod=false
+pnpm build
+pnpm export   # produces static "out/" folder
 echo "=== Deploying Next.js static site to $WEB_TARGET ==="
 rm -rf $WEB_TARGET/*
 cp -r out/* $WEB_TARGET/
-cd -
+cd $ROOT_DIR
 
 echo "=== Deployment completed successfully! ==="
